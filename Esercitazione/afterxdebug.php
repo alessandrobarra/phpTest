50ca60951ae0d4d0920c129998ebb4fc280c0570
Ale <alessandro.barra@spindox.it>
2023-02-20 16:34:10 +0100

    esercizio 1

--- Esercitazione/afterxdebug.php
+++ Esercitazione/afterxdebug.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 
 <?php 
-$cities = array()
+$cities = array("Tokyo", "Mexico City", "New York City" , "Mumbai", "Seoul", "Shanghai", "Lagos", "Buenos Aires", "Cairo", "London", "Los Angeles", "Calcutta" , "Osama", "Beijing");
 ?>
+<ul>
+<?php 
+foreach ($cities as $city){
+  echo "<li>" . $city . "<br>";
+}
+?>
+</ul>
